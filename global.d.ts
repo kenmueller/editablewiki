b5a29581bee5f7d2b5abbbda535b115fa12b1c66
Ken Mueller <kenmueller0@gmail.com>
2022-08-09 16:01:24 -0700

    get and save file

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="@sveltejs/kit" />
 
+interface ImportMetaEnv {
+	VITE_FIREBASE_STORAGE_BUCKET: string
+	VITE_FIREBASE_ADMIN_KEY: string
+}
+
 declare const VERSION: string
--- routes/[...path]/index.ts
+++ routes/[...path]/index.ts
@@ -1,12 +1,18 @@
 import type { RequestHandler } from '@sveltejs/kit'
+import { getStorage } from 'firebase-admin/storage'
 
+import admin from '$lib/firebase/admin'
 import errorFromValue from '$lib/error/from/value'
 
-export const GET: RequestHandler = ({ params: { path } }) => {
+const storage = getStorage(admin).bucket()
+
+export const GET: RequestHandler = async ({ params: { path } }) => {
 	try {
+		const [file] = await storage.file(encodeURIComponent(path)).get()
+
 		return {
 			headers: { 'content-type': 'text/html' },
-			body: `${path}<script src="/edit.js?v=${VERSION}" async></script>`
+			body: `${file.toString()}<script src="/edit.js?v=${VERSION}" async></script>`
 		}
 	} catch (value) {
 		const { code, message } = errorFromValue(value)
@@ -14,8 +20,16 @@ export const GET: RequestHandler = ({ params: { path } }) => {
 	}
 }
 
-export const POST: RequestHandler = ({ request, params: { path } }) => {
+export const POST: RequestHandler = async ({ request, params: { path } }) => {
 	try {
+		const html = await request.text()
+		const file = storage.file(encodeURIComponent(path))
+
+		await (html.trim()
+			? file.save(html, { contentType: 'text/html' })
+			: file.delete())
+
+		return { body: '' }
 	} catch (value) {
 		const { code, message } = errorFromValue(value)
 		return { status: code, body: message }
